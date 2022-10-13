8a8e7ee322d0722b9fccbbb4182f76c342eb0e27
Yuvraj Singh <ssplyuvraj@gmail.com>
2022-10-13 15:28:12 +0530

    Add code

---
+++ Asp_Net-Core-6-Web-Api-Setup/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Asp_Net_Core_6_Web_Api_Setup
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
